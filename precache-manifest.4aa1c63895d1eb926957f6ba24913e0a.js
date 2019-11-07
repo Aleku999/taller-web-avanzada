591adf0f4b1f9c57b9b3bef83ef3cbf809b2ea9e
Valetina <Aleku999@gmail.com>
2019-11-07 16:51:49 -0500

    Updates

---
+++ precache-manifest.4aa1c63895d1eb926957f6ba24913e0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8b6d07afb0a3f138d6d837d3656ba8",
+    "url": "/taller-web/index.html"
+  },
+  {
+    "revision": "582064a94f1f0acd778a",
+    "url": "/taller-web/static/css/main.62f53ecf.chunk.css"
+  },
+  {
+    "revision": "438b1837701217c654f6",
+    "url": "/taller-web/static/js/2.33ecbda3.chunk.js"
+  },
+  {
+    "revision": "582064a94f1f0acd778a",
+    "url": "/taller-web/static/js/main.8e6dee7e.chunk.js"
+  },
+  {
+    "revision": "497b79d667938f4d508a",
+    "url": "/taller-web/static/js/runtime-main.92d4c796.js"
+  }
+]);
